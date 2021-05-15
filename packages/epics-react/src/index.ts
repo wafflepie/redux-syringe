80af0e53905da60ffda481a5737d39633b0efb27
Václav Jančařík <vaclav.janc@gmail.com>
2021-05-15 10:05:39 +0200

    typescriptize epics-react

--- packages/epics-react/src/index.ts
+++ packages/epics-react/src/index.ts
@@ -1,5 +1,7 @@
+import type { Epic } from 'redux-observable';
+
 import { storeInterface } from '@redux-syringe/epics';
 import { makeHook, makeDecorator } from '@redux-syringe/injectors-react';
 
-export const useEpics = makeHook(storeInterface);
+export const useEpics = makeHook<Epic, typeof storeInterface>(storeInterface);
 export const withEpics = makeDecorator(storeInterface, useEpics);
