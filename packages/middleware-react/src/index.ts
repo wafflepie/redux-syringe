31e33b4e1140866207721cb5a7680cca6281cf4d
Václav Jančařík <vaclav.janc@gmail.com>
2021-05-15 10:39:01 +0200

    typescriptize middleware-react

--- packages/middleware-react/src/index.ts
+++ packages/middleware-react/src/index.ts
@@ -1,5 +1,7 @@
+import type { Middleware } from 'redux';
+
 import { makeHook, makeDecorator } from '@redux-syringe/injectors-react';
 import { storeInterface } from '@redux-syringe/middleware';
 
-export const useMiddleware = makeHook(storeInterface);
+export const useMiddleware = makeHook<Middleware, typeof storeInterface>(storeInterface);
 export const withMiddleware = makeDecorator(storeInterface, useMiddleware);
