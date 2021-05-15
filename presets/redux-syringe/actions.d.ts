fe98ae8d29d9d8505b19f35e2ee81a58483719ec
Václav Jančařík <vaclav.janc@gmail.com>
2021-05-15 15:42:20 +0200

    improve typing of actions

--- presets/redux-syringe/actions.d.ts
+++ presets/redux-syringe/actions.d.ts
@@ -1,11 +1,58 @@
 declare module '@redux-syringe/actions' {
-	export function isErrorAction(...args: any[]): any;
-	export function isNotErrorAction(...args: any[]): any;
-	export function configureActionCreator(...args: any[]): any;
-	export function makeActionTypes(...args: any[]): any;
-	export function makeEmptyActionCreator(...args: any[]): any;
-	export function makePayloadActionCreator(...args: any[]): any;
-	export function makeConstantActionCreator(...args: any[]): any;
-	export function makeSimpleActionCreator(...args: any[]): any;
-	export function makePayloadMetaActionCreator(...args: any[]): any;
+	/**
+	 * @deprecated Use different action types instead of relying on an `error` property.
+	 */
+	export const isErrorAction: (action: any) => action is { error: true };
+
+	/**
+	 * @deprecated Use different action types instead of relying on an `error` property.
+	 */
+	export const isNotErrorAction: (action: any) => boolean;
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const configureActionCreator: (...args: any[]) => any;
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makeActionTypes: <S extends string, U extends string>(
+		scope: S,
+		types: U[]
+	) => { [K in U]: `${S}/${K}` };
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makeEmptyActionCreator: <T extends string>(type: T) => () => { type: T };
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makeConstantActionCreator: <T extends string>(type: T) => () => { type: T };
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makePayloadActionCreator: <T extends string>(
+		type: T
+	) => <P>(payload: P) => { payload: P; type: T };
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makeSimpleActionCreator: <T extends string>(
+		type: T
+	) => <P>(payload: P) => { payload: P; type: T };
+
+	/**
+	 * @deprecated Use `createAction` from Redux Toolkit.
+	 */
+	export const makePayloadMetaActionCreator: <T extends string>(
+		type: T
+	) => <P, M extends Record<string, unknown>>(
+		payload: P,
+		meta: M
+	) => { meta: M; payload: P; type: T };
 }
